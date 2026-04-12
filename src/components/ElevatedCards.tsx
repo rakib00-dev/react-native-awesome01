398e0e54fbf917719cc5dc033999a9179ca100f7
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-12 18:06:29 +0600

    added new section called elevated cards

---
+++ src/components/ElevatedCards.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ElevatedCards = () => {
+  return (
+    <View>
+      <Text>ElevatedCards</Text>
+    </View>
+  );
+};
+
+export default ElevatedCards;
+
+const styles = StyleSheet.create({});
